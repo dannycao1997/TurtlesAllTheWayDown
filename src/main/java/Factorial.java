01e2a2d06f79115097f735a643526fd6a54ff850
Danny Cao <dannycao1997@yahoo.com>
2024-04-08 10:46:25 -0400

    updated gdc

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -1,5 +1,5 @@
 public class Factorial {
-    public static int factorial(int n){
+    public static long factorial(int n){
         if (n == 0) return 1;
         return n * factorial(n - 1);
     }
--- src/main/java/GCD.java
+++ src/main/java/GCD.java
@@ -1,2 +1,6 @@
-public class GCD {
+public class GDC {
+    public static boolean gcd(int p, int q) {
+        if (q == 0) return (p == 1);
+        return gcd(q,p % q);
+    }
 }
